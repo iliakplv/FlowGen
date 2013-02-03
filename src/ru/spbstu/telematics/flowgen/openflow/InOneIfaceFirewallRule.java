f4490395ea721da4226a739043d291e7254519d6
iliakplv <iliakplv@gmail.com>
2013-02-03 20:00:26 +0400

    Rules hierarchy

---
+++ src/ru/spbstu/telematics/flowgen/openflow/InOneIfaceFirewallRule.java
@@ -0,0 +1,10 @@
+package ru.spbstu.telematics.flowgen.openflow;
+
+
+public class InOneIfaceFirewallRule extends OneIfaceFirewallRule {
+
+	public InOneIfaceFirewallRule(String dpid, int inPort, int firewallPort) {
+		super(dpid, inPort, firewallPort);
+	}
+
+}
--- src/ru/spbstu/telematics/flowgen/openflow/OneIfaceFirewallRule.java
+++ src/ru/spbstu/telematics/flowgen/openflow/OneIfaceFirewallRule.java
@@ -1,11 +1,33 @@
 package ru.spbstu.telematics.flowgen.openflow;
 
 
-import java.util.Set;
+import ru.spbstu.telematics.flowgen.utils.OpenflowUtils;
 
-public class OneIfaceFirewallRule extends FirewallRule {
 
-	public OneIfaceFirewallRule(String dpid, int inPort, Set<Integer> outPorts) {
-		super(dpid, inPort, outPorts);
+public abstract class OneIfaceFirewallRule extends FirewallRule {
+
+	private int mFirewallPort;
+
+
+	public OneIfaceFirewallRule(String dpid, int inPort, int firewallPort) {
+		super(dpid, inPort);
+		setFirewallPort(firewallPort);
+	}
+
+
+	/**
+	 * Firewall Port
+	 *
+	 */
+
+	public int getFirewallPort() {
+		return mFirewallPort;
+	}
+
+	public void setFirewallPort(int firewallPort) {
+		if (!OpenflowUtils.validatePortNumber(firewallPort)) {
+			throw new IllegalArgumentException("Wrong port number");
+		}
+		mFirewallPort = firewallPort;
 	}
 }
