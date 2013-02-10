af1d881b1f305ab3e8b23da3f8531f08333bf2c1
ilya <iliakplv@gmail.com>
2013-02-10 13:38:51 +0400

    OpenflowUtils labels

--- src/ru/spbstu/telematics/flowgen/utils/OpenflowUtils.java
+++ src/ru/spbstu/telematics/flowgen/utils/OpenflowUtils.java
@@ -3,21 +3,32 @@ package ru.spbstu.telematics.flowgen.utils;
 
 public class OpenflowUtils {
 
+	// Ports
 	public static final int MIN_PORT = 1;
 	public static final int MAX_PORT = 255;
 
+	// Rule components labels
+	public static final String RULE_DPID = 				"switch";
+	public static final String RULE_FLOW_NAME = 		"name";
+	public static final String RULE_PRIORITY = 			"priority";
+	public static final String RULE_ACTIVE = 			"active";
+	public static final String RULE_IN_PORT = 			"ingress_port";
+	public static final String RULE_ACTIONS = 			"actions";
+	public static final String RULE_OUT_PORTS_PREFIX =	"output=";
+
+
+	// Rule priority
+	public static final int DEFAULT_RULE_PRIORITY = 32767;
+	public static final int MAX_RULE_PRIORITY = 32767;
+	public static final int MIN_RULE_PRIORITY = 0;
+
+	// Other
 	public static final int DPID_BYTES = 8;
 	public static final int MAC_BYTES = 6;
 
 	public static final char DPID_DELIMITER = ':';
 	public static final char MAC_DELIMITER = ':';
 	public static final char PORTS_DELIMITER = ',';
-
-	// TODO check these values
-	public static final int DEFAULT_RULE_PRIORITY = 32000;
-	public static final int MAX_RULE_PRIORITY = 32000;
-	public static final int MIN_RULE_PRIORITY = 0;
-
 
 
 	public static boolean validateDpid(String dpid) {
