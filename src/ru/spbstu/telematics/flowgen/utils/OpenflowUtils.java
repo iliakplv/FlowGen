4af0baaccf489a333390aa9d535f0e9d142477ee
ilya <iliakplv@gmail.com>
2013-02-10 13:40:38 +0400

    OpenflowUtils labels

--- src/ru/spbstu/telematics/flowgen/utils/OpenflowUtils.java
+++ src/ru/spbstu/telematics/flowgen/utils/OpenflowUtils.java
@@ -3,10 +3,6 @@ package ru.spbstu.telematics.flowgen.utils;
 
 public class OpenflowUtils {
 
-	// Ports
-	public static final int MIN_PORT = 1;
-	public static final int MAX_PORT = 255;
-
 	// Rule components labels
 	public static final String RULE_DPID = 				"switch";
 	public static final String RULE_FLOW_NAME = 		"name";
@@ -16,16 +12,19 @@ public class OpenflowUtils {
 	public static final String RULE_ACTIONS = 			"actions";
 	public static final String RULE_OUT_PORTS_PREFIX =	"output=";
 
+	// Ports
+	public static final int MIN_PORT = 1;
+	public static final int MAX_PORT = 255;
 
 	// Rule priority
 	public static final int DEFAULT_RULE_PRIORITY = 32767;
 	public static final int MAX_RULE_PRIORITY = 32767;
 	public static final int MIN_RULE_PRIORITY = 0;
 
+
 	// Other
 	public static final int DPID_BYTES = 8;
 	public static final int MAC_BYTES = 6;
-
 	public static final char DPID_DELIMITER = ':';
 	public static final char MAC_DELIMITER = ':';
 	public static final char PORTS_DELIMITER = ',';
