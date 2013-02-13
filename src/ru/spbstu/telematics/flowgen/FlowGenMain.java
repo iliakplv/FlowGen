b35b5af98f925fa84aeb6de785c7441e6cd20240
iliakplv <iliakplv@gmail.com>
2013-02-13 11:46:12 +0400

    http test

--- src/ru/spbstu/telematics/flowgen/FlowGenMain.java
+++ src/ru/spbstu/telematics/flowgen/FlowGenMain.java
@@ -1,8 +1,18 @@
 package ru.spbstu.telematics.flowgen;
 
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import ru.spbstu.telematics.flowgen.openflow.FirewallRule;
 import ru.spbstu.telematics.flowgen.openflow.OneIfaceFirewallRule;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
 
 public class FlowGenMain {
 
@@ -10,11 +20,23 @@ public class FlowGenMain {
 
 		FirewallRule rule = new OneIfaceFirewallRule("00:00:00:00:00:00:00:01", 1, 2, "12:34:65:78:9A:B0");
 
-		System.out.println(rule.ovsInFlowAddCommand().toString());
-		System.out.println(rule.ovsOutFlowAddCommand().toString());
+		HttpClient httpClient = new DefaultHttpClient();
+		try {
+			HttpPost request = new HttpPost("http://localhost:8080/wm/staticflowentrypusher/json");
+			StringEntity params = new StringEntity(rule.ovsInFlowAddCommand().toString());
+			request.addHeader("content-type", "application/x-www-form-urlencoded");
+			request.setEntity(params);
+			HttpResponse response = httpClient.execute(request);
 
-		System.out.println(rule.ovsInFlowRemoveCommand().toString());
-		System.out.println(rule.ovsOutFlowRemoveCommand().toString());
+			// ...
+		} catch (UnsupportedEncodingException e) {
+			// ...
+		} catch (IOException e) {
+			// ...
+		} finally {
+			httpClient.getConnectionManager().shutdown();
+		}
+
 
 
 	}
