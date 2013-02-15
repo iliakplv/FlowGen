1c074eb2e95b08dffb0ca98a9d12ba4f0b6aa20f
iliakplv <iliakplv@gmail.com>
2013-02-15 11:40:00 +0400

    test

--- src/ru/spbstu/telematics/flowgen/FlowGenMain.java
+++ src/ru/spbstu/telematics/flowgen/FlowGenMain.java
@@ -1,7 +1,6 @@
 package ru.spbstu.telematics.flowgen;
 
 
-import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
@@ -18,26 +17,39 @@ public class FlowGenMain {
 
 	public static void main(String[] args) {
 
-		FirewallRule rule = new OneIfaceFirewallRule("00:00:00:00:00:00:00:01", 1, 2, "12:34:65:78:9A:B0");
+		String sfpUrl =	"http://192.168.168.24:8080/wm/staticflowentrypusher/json";
+		String dpid = "00:00:b6:60:ff:e5:93:4f";
+		int fwPort = 1;
+
+		int[] vmPorts = new int[]	{2};
+		String[] vmMacs = 			{"12:34:65:78:9A:B0"};
 
 		HttpClient httpClient = new DefaultHttpClient();
 		try {
-			HttpPost request = new HttpPost("http://localhost:8080/wm/staticflowentrypusher/json");
-			StringEntity params = new StringEntity(rule.ovsInFlowAddCommand().toString());
-			request.addHeader("content-type", "application/x-www-form-urlencoded");
-			request.setEntity(params);
-			HttpResponse response = httpClient.execute(request);
+			for (int i = 0; i < vmPorts.length; i++) {
+				OneIfaceFirewallRule rule = new OneIfaceFirewallRule(dpid, fwPort, vmPorts[i], vmMacs[i]);
 
-			// ...
+				HttpPost inRequest = new HttpPost(sfpUrl);
+				inRequest.addHeader("content-type", "application/x-www-form-urlencoded");
+				StringEntity inParams = new StringEntity(rule.ovsInFlowAddCommand().toString());
+				inRequest.setEntity(inParams);
+				httpClient.execute(inRequest);
+
+				HttpPost outRequest = new HttpPost(sfpUrl);
+				inRequest.addHeader("content-type", "application/x-www-form-urlencoded");
+				StringEntity outParams = new StringEntity(rule.ovsOutFlowAddCommand().toString());
+				outRequest.setEntity(outParams);
+				httpClient.execute(outRequest);
+			}
+
 		} catch (UnsupportedEncodingException e) {
-			// ...
+			e.printStackTrace();
 		} catch (IOException e) {
-			// ...
+			e.printStackTrace();
 		} finally {
 			httpClient.getConnectionManager().shutdown();
 		}
 
-
-
 	}
+
 }
