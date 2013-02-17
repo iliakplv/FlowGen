1e77bc7d613fb8ed34bcd7bfa5c0e30d479e5e62
ilya <iliakplv@gmail.com>
2013-02-17 16:42:21 +0400

    HttpDeleteWithBody

---
+++ src/ru/spbstu/telematics/flowgen/httpclient/HttpDeleteWithBody.java
@@ -0,0 +1,32 @@
+package ru.spbstu.telematics.flowgen.httpclient;
+
+
+import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
+
+import java.net.URI;
+
+
+class HttpDeleteWithBody extends HttpEntityEnclosingRequestBase {
+
+	public static final String METHOD_NAME = "DELETE";
+
+
+	public HttpDeleteWithBody(final String uri) {
+		super();
+		setURI(URI.create(uri));
+	}
+
+	public HttpDeleteWithBody(final URI uri) {
+		super();
+		setURI(uri);
+	}
+
+	public HttpDeleteWithBody() {
+		super();
+	}
+
+	public String getMethod() {
+		return METHOD_NAME;
+	}
+
+}
