571901ea4dfcabfc4fd575ff62795a6510d8ddb4
iliakplv <iliakplv@gmail.com>
2013-02-26 10:40:18 +0400

    execute post

--- src/ru/spbstu/telematics/flowgen/FlowGenMain.java
+++ src/ru/spbstu/telematics/flowgen/FlowGenMain.java
@@ -1,13 +1,22 @@
 package ru.spbstu.telematics.flowgen;
 
 
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONObject;
 import ru.spbstu.telematics.flowgen.openflow.Datapath;
 import ru.spbstu.telematics.flowgen.openflow.IDatapath;
+import ru.spbstu.telematics.flowgen.openflow.OnePortFirewallRule;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 
 public class FlowGenMain {
+
+	// TODO LOG
 
 	public static void main(String[] args) {
 
@@ -65,41 +74,25 @@ public class FlowGenMain {
 			System.out.println(snCommands[i].toString());
 		}
 
-		// TODO LOG
+	}
 
-//		String sfpUrl =	"http://192.168.168.24:8080/wm/staticflowentrypusher/json";
-//		String dpid = "00:00:b6:60:ff:e5:93:4f";
-//		int fwPort = 1;
-//
-//		int[] vmPorts = new int[]	{2};
-//		String[] vmMacs = 			{"12:34:65:78:9A:B0"};
-//
-//		HttpClient httpClient = new DefaultHttpClient();
-//		try {
-//			for (int i = 0; i < vmPorts.length; i++) {
-//				OnePortFirewallRule rule = new OnePortFirewallRule(dpid, fwPort, vmPorts[i], vmMacs[i]);
-//
-//				HttpPost inRequest = new HttpPost(sfpUrl);
-//				inRequest.addHeader("content-type", "application/x-www-form-urlencoded");
-//				StringEntity inParams = new StringEntity(rule.ovsInFlowAddCommand().toString());
-//				inRequest.setEntity(inParams);
-//				httpClient.execute(inRequest);
-//
-//				HttpPost outRequest = new HttpPost(sfpUrl);
-//				inRequest.addHeader("content-type", "application/x-www-form-urlencoded");
-//				StringEntity outParams = new StringEntity(rule.ovsOutFlowAddCommand().toString());
-//				outRequest.setEntity(outParams);
-//				httpClient.execute(outRequest);
-//			}
-//
-//		} catch (UnsupportedEncodingException e) {
-//			e.printStackTrace();
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		} finally {
-//			httpClient.getConnectionManager().shutdown();
-//		}
+	public static void executePost(JSONObject command) {
+		final String url =	"http://192.168.168.24:8080/wm/staticflowentrypusher/json";
 
+		HttpClient httpClient = new DefaultHttpClient();
+		try {
+			HttpPost request = new HttpPost(url);
+			request.addHeader("content-type", "application/x-www-form-urlencoded");
+			StringEntity inParams = new StringEntity(command.toString());
+			request.setEntity(inParams);
+			httpClient.execute(request);
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			httpClient.getConnectionManager().shutdown();
+		}
 	}
 
 }
