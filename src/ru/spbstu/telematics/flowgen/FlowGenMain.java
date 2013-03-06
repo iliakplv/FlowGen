f43aba3150d2ad4cb0f5e384eadb8d5f3dac2f8d
iliakplv <iliakplv@gmail.com>
2013-03-06 11:57:27 +0400

    test on vn0

--- src/ru/spbstu/telematics/flowgen/FlowGenMain.java
+++ src/ru/spbstu/telematics/flowgen/FlowGenMain.java
@@ -7,6 +7,11 @@ import ru.spbstu.telematics.flowgen.openflow.IDatapath;
 import ru.spbstu.telematics.flowgen.openflow.IStaticFlowPusherClient;
 import ru.spbstu.telematics.flowgen.openflow.StaticFlowPusherClient;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+
 
 public class FlowGenMain {
 
@@ -14,62 +19,33 @@ public class FlowGenMain {
 
 	public static void main(String[] args) {
 
-		IStaticFlowPusherClient sfpClient = new StaticFlowPusherClient("127.0.0.1", 8080);
-		System.out.println("\nSFP at " + ((StaticFlowPusherClient)sfpClient).getStaticFlowPusherUrl() + "\n");
+		// Datapath, Gateway
 
-		String dpid = "00:00:b6:60:ff:e5:93:4f";
-		String gwMac = "FF:FF:AA:AA:FF:FF";
+		String dpid = "00:00:a6:49:24:26:a5:40";
 		int trunkPort = 1;
-		int firewallPort = 2;
-		IDatapath dp = new Datapath(dpid,"ovs-network", trunkPort, firewallPort, gwMac);
+		int firewallPort = 3;
+		String gwMac = "fa:16:3e:15:2d:df";
+		IDatapath datapath = new Datapath(dpid, "qbr1dee26dc-b0", trunkPort, firewallPort, gwMac);
 
-		int vmPort = 3;
-		int vmMacLastByte = 13;
+		// VMs
+
+		HashMap<Integer, String> vmPortMacMap = new HashMap<Integer, String>();
+		vmPortMacMap.put(4, "fa:16:3e:69:ab:bf");
+		vmPortMacMap.put(5, "fa:16:3e:38:0f:e9");
 
 
-		System.out.println(" ===== Connect all to " + dp.toString() + " =====\n");
+		// Adding flows
 
-		for (int i = 0; i < 3; i++) {
-			JSONObject[] commands = dp.connectVm("00:00:00:00:00:" + vmMacLastByte++, vmPort++);
-			for (int j = 0; j < commands.length; j++) {
-				System.out.println(commands[j].toString());
-			}
-			System.out.println();
+		IStaticFlowPusherClient sfpClient = new StaticFlowPusherClient("localhost", 8080);
+
+		sfpClient.addFlows(datapath.connectGateway());
+		sfpClient.addFlows(datapath.connectSubnet());
+
+		Set<Integer> ports = vmPortMacMap.keySet();
+		for (int port : ports) {
+			sfpClient.addFlows(datapath.connectVm(vmPortMacMap.get(port), port));
 		}
 
-		JSONObject[] gwCommands = dp.connectGateway();
-		for (int i = 0; i < gwCommands.length; i++) {
-			System.out.println(gwCommands[i].toString());
-		}
-		System.out.println();
-
-		JSONObject[] snCommands = dp.connectSubnet();
-		for (int i = 0; i < snCommands.length; i++) {
-			System.out.println(snCommands[i].toString());
-		}
-
-
-		System.out.println("\n ===== Disconnect all from " + dp.toString() + " =====\n");
-
-		for (int i = 0; i < 3; i++) {
-//			JSONObject[] commands = dp.disconnectVm("00:00:00:00:00:" + --vmMacLastByte);
-			JSONObject[] commands = dp.disconnectVm(--vmPort);
-			for (int j = 0; j < commands.length; j++) {
-				System.out.println(commands[j].toString());
-			}
-			System.out.println();
-		}
-
-		gwCommands = dp.disconnectGateway();
-		for (int i = 0; i < gwCommands.length; i++) {
-			System.out.println(gwCommands[i].toString());
-		}
-		System.out.println();
-
-		snCommands = dp.disconnectSubnet();
-		for (int i = 0; i < snCommands.length; i++) {
-			System.out.println(snCommands[i].toString());
-		}
 
 	}
 
